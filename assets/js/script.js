ff32c286135719eaf6098ce170355be56dfc3f48
ROMANELLO Juanita <juanitaromanello@gmail.com>
2022-08-02 18:19:42 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function invert(){
+    let imgdavid = document.getElementById("imgdavid")
+    let imgdavid2 = document.getElementById("imgdavid2")
+    let imgjuans = document.getElementById("imgjuans")
+    let imgjuans2 = document.getElementById("imgjuans2")
+    let imgjuanp = document.getElementById("imgjuanp")
+    let imgjuanp2 = document.getElementById("imgjuanp2")
+    let imgjuani = document.getElementById("imgjuani")
+    let imgjuani2 = document.getElementById("imgjuani2")
+    let html = document.getElementById("html")
+    let inputColor = document.getElementById("inputColor");
+    html.classList.toggle("invertColor");
+    imgdavid.classList.toggle("hide");
+    imgdavid2.classList.toggle("hide");
+    imgjuans.classList.toggle("hide");
+    imgjuans2.classList.toggle("hide");
+    imgjuanp.classList.toggle("hide");
+    imgjuanp2.classList.toggle("hide");
+    imgjuani.classList.toggle("hide");
+    imgjuani2.classList.toggle("hide");
+    }
+
